8e0cfaf5a7665e952b061d4e03afa07a3e08ccea
Gabriel Rosas <therealtheriel@gmail.com>
2026-05-31 20:52:54 -0700

    score calculation first iteration

--- test_model.py
+++ test_model.py
@@ -1,5 +1,13 @@
+from typing import Any
+
 from transformers import pipeline
 from argparse import ArgumentParser
+
+def calc_score(result: list[dict[str, Any]]) -> float:
+    total = 0.0
+    for i, score in enumerate(sorted(result, key=lambda content: int(content['label'][-1]))):
+        total += score['score'] * (i + 1)
+    return (total - 1) / len(result)
 
 def main():
     parser = ArgumentParser("test_model")
@@ -13,9 +21,11 @@ def main():
         model=f"./{args.model}"
     )
 
-    result = classifier(args.input)
+    result = classifier(args.input, top_k=None)
 
     print(result)
+    print(sorted(result, key=lambda content: int(content['label'][-1])))
+    print(calc_score(result))
 
 if __name__ == "__main__":
     main()
